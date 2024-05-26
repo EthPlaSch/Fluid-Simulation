62d568cfdd35b7430f192ec9e40266fbc2754fd8
EthPlaSch <ethan.platt@stu.burgmann.act.edu.au>
2024-05-26 16:21:12 +1000

    Trig collisions - Maybe an option
    Two particles (particle 1 is blue and particle 2 is red I'm pretty sure), "colliding" using sin and cos. Basically if the red ball is on the left the x gives -1 and if its on the right it gives +1, same for sin and the y axis. Currently draws a yellow dot at the "point of collision".

--- Fluid Simulation.py
+++ Fluid Simulation.py
@@ -1,16 +1,16 @@
 import pyglet
 from pyglet import shapes
-from math import hypot
+from math import hypot, atan, acos, asin
 
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 360
 GRAVITY = 9.8
-NUMBER_OF_PARTICLES = 10
-PARTICLE_RADIUS = 10
+NUMBER_OF_PARTICLES = 2
+PARTICLE_RADIUS = 20
 SPACING = -2
 # Sweetie 16 Palette
 PARTICLE_COLOUR = (65, 166, 246)
-COLLISION_FORCE = 400
+COLLISION_FORCE = 40
 DT_SCALE = 1
 ELASTICTY = 0.2
 
@@ -27,8 +27,8 @@ shapes_batch = pyglet.graphics.Batch()
 # Particle Class
 class Particle:
     
-    def __init__(self, index, offset, radius = PARTICLE_RADIUS, x = WINDOW_WIDTH / 2, y = WINDOW_HEIGHT / 2) -> None:
-        self.circle = shapes.Circle(x = x + offset, y = y + offset, radius = PARTICLE_RADIUS, color = PARTICLE_COLOUR, batch = shapes_batch)
+    def __init__(self, index, offset = 0, colour = PARTICLE_COLOUR, radius = PARTICLE_RADIUS, x = WINDOW_WIDTH / 2, y = WINDOW_HEIGHT / 2) -> None:
+        self.circle = shapes.Circle(x = x + (offset / 2.01), y = y + offset, radius = PARTICLE_RADIUS, color = colour, batch = shapes_batch)
         self.circle.opacity = 200
         self.index = index
         self.velocity = [0, 0]
@@ -36,23 +36,17 @@ class Particle:
     def update_ball(self, dt):
         dt = dt/DT_SCALE
         self.velocity[1] -= GRAVITY
-        # self.circle.y -= self.speed
-        
         # Keep in the boundary
         if self.circle.y < self.circle.radius:
             self.circle.y = self.circle.radius
-            # if self.speed < 3:
-            #     self.speed = 0
-            # else:
-            #     # Bounce
             self.velocity[1] *= -ELASTICTY
         if self.circle.y > WINDOW_HEIGHT - self.circle.radius:
             self.circle.y = WINDOW_HEIGHT - self.circle.radius
             self.velocity[1] *= -ELASTICTY
-        if self.circle.x < 0:
+        if self.circle.x < self.circle.radius:
             self.circle.x = self.circle.radius
             self.velocity[0] *= -ELASTICTY
-        elif self.circle.x > WINDOW_WIDTH:
+        if self.circle.x > WINDOW_WIDTH - self.circle.radius:
             self.circle.x = WINDOW_WIDTH - self.circle.radius
             self.velocity[0] *= -ELASTICTY
                 
@@ -68,10 +62,15 @@ background = shapes.Rectangle(x = 0, y = 0, width = WINDOW_WIDTH, height = WINDO
 # Generating some particles (change later)
 offset = -(NUMBER_OF_PARTICLES * PARTICLE_RADIUS * 2 + (SPACING * NUMBER_OF_PARTICLES)) / 2
 
-for i in range(NUMBER_OF_PARTICLES):
-    particles.append(Particle(i, offset))
-    particle_index_pos[i] = (particles[i].circle.x, particles[i].circle.y)
-    offset += PARTICLE_RADIUS * 2 + SPACING
+# for i in range(NUMBER_OF_PARTICLES):
+#     particles.append(Particle(i, offset))
+#     particle_index_pos[i] = (particles[i].circle.x, particles[i].circle.y)
+#     offset += PARTICLE_RADIUS * 2 + SPACING
+
+particles.append(Particle(0, 50))
+particles.append(Particle(1, 0, (177, 62, 83)))
+particle_index_pos[0] = (particles[0].circle.x, particles[0].circle.y)
+particle_index_pos[1] = (particles[1].circle.x, particles[1].circle.y)
 
 # Running the actual functions
 @window.event
@@ -97,36 +96,19 @@ def update(dt):
             if distance <= 2 * PARTICLE_RADIUS:
                 if abs(distance) == 0:
                     distance = 0.0000000000000000000000000000000000000000000000000001
-                # particle_vel_x = particles[i].velocity[0]
-                # particle_vel_y = particles[i].velocity[1]
-                # other_vel_x = particles[j].velocity[0]
-                # other_vel_y = particles[j].velocity[1]
-                # dist_x = x1 - x2
-                # dist_y = y1 - y2
-                # dot_prod = (dist_x*particle_vel_x + dist_y*particle_vel_y) / (dist_x**2 + dist_y**2)
                 
-                # particles[i].velocity[0] += 2 * dist_x * dot_prod
-                # particles[i].velocity[1] += 2 * dist_y * dot_prod
-                # particles[j].velocity[0] = 0
-                # particles[j].velocity[1] = 0
-                if (x2 - x1) <= 0: # j to the left of i
-                    particles[i].velocity[0] += (COLLISION_FORCE/abs(distance))
-                    particles[j].velocity[0] -= (COLLISION_FORCE/abs(distance))
-                else: # j to the right of i
-                    particles[i].velocity[0] -= (COLLISION_FORCE/abs(distance))
-                    particles[j].velocity[0] += (COLLISION_FORCE/abs(distance))
-                    
-                if (y2 - y1) <= 0:
-                    particles[i].velocity[1] += (COLLISION_FORCE/abs(distance))
-                    particles[j].velocity[0] -= (COLLISION_FORCE/abs(distance))
-                else:
-                    particles[i].velocity[1] -= (COLLISION_FORCE/abs(distance))
-                    particles[j].velocity[1] += (COLLISION_FORCE/abs(distance))
+                point_of_collision_x = ((x2 - x1) / (PARTICLE_RADIUS * 2))
+                point_of_collision_y = ((y2 - y1) / (PARTICLE_RADIUS * 2))
+                print(f'X collision: {point_of_collision_x}, Y collision: {point_of_collision_y}')
                 
-                # Later, use math to find the point of collision on the circle and apply a force in that direction to push the other particles away
+                # Drawing the point where the circles collide
+                global point
+                point = shapes.Circle(x = particles[0].circle.x + point_of_collision_x * PARTICLE_RADIUS, y = particles[0].circle.y + point_of_collision_y * PARTICLE_RADIUS, radius = 3, color = (255, 205, 117), batch = shapes_batch)
+                point.draw()
                 
         particles[i].update_ball(dt)
     particles[-1].update_ball(dt)
+    
 
 # Basically saying call the update function 60 times per second
 pyglet.clock.schedule_interval(update, 1/60)
