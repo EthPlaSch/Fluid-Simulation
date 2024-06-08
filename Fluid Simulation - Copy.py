ffa7dd9949e1762fe3b420750766a82f61ac749c
EthPlaSch <ethan.platt@stu.burgmann.act.edu.au>
2024-06-08 21:25:36 +1000

    Better looking water
    + Deleted other weird files
    + Way better looking water by blurring the particles

--- Fluid Simulation - Copy.py
+++ Fluid Simulation - Copy.py
@@ -19,7 +19,6 @@ SEPERATION_FACTOR = 2
 def dot_2d(x1, x2, y1, y2):
     return (x1*x2) + (y1*y2)
     
-
 particles = []
 particle_index_pos = {}
 
@@ -30,23 +29,28 @@ window.set_location(440, 240)
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 # Batch for rendering
-shapes_batch = pyglet.graphics.Batch()
+sprites_batch = pyglet.graphics.Batch()
+
+# Loading in the image used to simulate the blur
+particle_image = pyglet.image.load("Particle.png")
+
+images = []
 
 # Particle Class
 class Particle:
     
     def __init__(self, index, colour = PARTICLE_COLOUR, x = WINDOW_WIDTH / 2, y = WINDOW_HEIGHT / 2) -> None:
-        self.circle = shapes.Circle(x = x, y = y, radius = PARTICLE_RADIUS, color = colour, batch = shapes_batch)
+        self.circle = shapes.Circle(x = x, y = y, radius = PARTICLE_RADIUS, color = colour)
         self.circle.opacity = 200
         self.index = index
         self.velocity = [0, 0]
 
-    # Function to update the balls position each frame
-    def update_ball(self, dt):
+    # Function to update the particle's position each frame
+    def update_particle(self, dt):
         dt = dt/DT_SCALE
         self.velocity[1] -= GRAVITY
 
-        # Keep in the boundary | if the ball hits an edge move it back inside the boundary and reverse its velocity
+        # Keep in the boundary | if the particle hits an edge move it back inside the boundary and reverse its velocity
         if self.circle.y <= self.circle.radius:
             self.circle.y = self.circle.radius
             self.velocity[1] *= -ELASTICTY
@@ -65,13 +69,17 @@ class Particle:
         self.circle.y += self.velocity[1] * dt
     
 # Background rectangle
-background = shapes.Rectangle(x = 0, y = 0, width = WINDOW_WIDTH, height = WINDOW_HEIGHT, color = (26, 28, 44), batch = shapes_batch)
+background = shapes.Rectangle(x = 0, y = 0, width = WINDOW_WIDTH, height = WINDOW_HEIGHT, color = (26, 28, 44))
 
 colour = int(255 / NUMBER_OF_PARTICLES)
 
+# Generating the particles
 for i in range(NUMBER_OF_PARTICLES):
     particles.append(Particle(i, (65, 166, 246), i, i))
     particle_index_pos[i] = (particles[i].circle.x, particles[i].circle.y)
+    single_particle_image = pyglet.sprite.Sprite(particle_image, particles[i].circle.x, particles[i].circle.y, batch = sprites_batch)
+    single_particle_image.scale = 0.022
+    images.append(single_particle_image)
     
 # Running the actual functions
 @window.event
@@ -80,13 +88,16 @@ def on_draw():
     # Clearing the window
     window.clear()
     
-    # Drawing all the shapes in the batch
-    shapes_batch.draw()
+    # Drawing the background
+    background.draw()
+
+    # Drawing the sprites 
+    sprites_batch.draw()
     
 # Update function
 def update(dt):
     
-    # Getting 
+    # Getting key input to impart force on the particles
     if keys[key.SPACE]:
         for i in range(len(particles)):
             particles[i].velocity[1] += 40
@@ -96,11 +107,14 @@ def update(dt):
     if keys[key.RIGHT]:
         for i in range(len(particles)):
             particles[i].velocity[0] += 10
+            
+    # Updating the delta time to be in slomo or not
     dt = dt/DT_SCALE
     
     for particle in particles:
         particle_index_pos[particle.index] = (particles[particle.index].circle.x, particles[particle.index].circle.y)
         
+    # Looping over every particle
     for i in range(len(particles) - 1):
         particle_pos = particle_index_pos[i]
         # Loop over every other particle
@@ -111,7 +125,10 @@ def update(dt):
             # Calculate the distance between them
             distance = hypot(abs(x1 - x2), abs(y1 - y2))
             
+            # Checking if the particles collided
             if distance <= 2 * PARTICLE_RADIUS:
+                
+                # Avoiding divide by 0 error
                 if abs(distance) == 0:
                     distance = 0.0000000000000000000000000000000000000000000000000001
                 
@@ -130,13 +147,23 @@ def update(dt):
                 particles[j].velocity[1] += (p * norm_of_vector_y)
                 # -----------------------------------------------------------------------------------------------------------------------------
                 
+                # This forces the particles apart by moving the particles apart along the normalised vectors
                 particles[i].circle.x -= norm_of_vector_x * SEPERATION_FACTOR
                 particles[i].circle.y -= norm_of_vector_y * SEPERATION_FACTOR
                 particles[j].circle.x += norm_of_vector_x * SEPERATION_FACTOR
                 particles[j].circle.y += norm_of_vector_y * SEPERATION_FACTOR
-                
+              
+        # Updating the position of the sprite to match the position of the particle  
+        images[i].x = particles[i].circle.x - PARTICLE_RADIUS * 2.2
+        images[i].y = particles[i].circle.y - PARTICLE_RADIUS * 2.2
+        
+    # Updating the last particle's image location
+    images[-1].x = particles[-1].circle.x - PARTICLE_RADIUS * 2.2
+    images[-1].y = particles[-1].circle.y - PARTICLE_RADIUS * 2.2
+             
+    # Looping over every particle and updating them   
     for i in range(len(particles)):
-        particles[i].update_ball(dt)
+        particles[i].update_particle(dt)
     
 
 # Basically saying call the update function 60 times per second
